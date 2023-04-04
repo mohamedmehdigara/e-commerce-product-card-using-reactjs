48b72c3a1db7b3e4af8fb30b183a7daa007e21fe
mohamedmehdigara <garamehdi57@yahoo.com>
2023-04-04 14:38:17 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css"
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/batman.png"
+          old_price="9,999"
+          newPrice="9999"
+          dollar="$"
+          alt="batman"
+          exp_date="10-08-2022"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/blackpanter.png"
+          old_price="599"
+          newPrice="500"
+          dollar="$"
+          alt="blackpanter"
+          exp_date="10-08-2022"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/arthur.png"
+          old_price="7999"
+          newPrice="7000"
+          dollar="$"
+          alt="arthur"
+          exp_date="10-08-2022"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/kashima.png"
+          old_price="999"
+          newPrice="500"
+          dollar="$"
+          alt="kashima"
+          exp_date="10-08-2022"
+        />
+      </div>
     </div>
   );
 }
-
 export default App;
